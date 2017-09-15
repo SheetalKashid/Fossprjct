81290e1c9f4856b2eb92f13349d78c29239ed920
SheetalKashid <kashid.sheetal3@gmail.com>
2017-09-15 11:13:39 +0530

    this is my first change in my foss project

---
+++ arithmetic.py
@@ -0,0 +1,10 @@
+a=int(input("enter value of a"))
+b=int(input("enter value of b"))
+c=a+b
+print("addition of a and b is:",c)
+c=a-b
+print("subtraction of a and b is:",c)
+c=a*b
+print("multiplication of a and b is:",c)
+c=a/b
+print("division of a and b is:",c)
